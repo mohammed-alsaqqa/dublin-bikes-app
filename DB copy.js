8ed19058ddf5fb333d27ef17586cf73ad29c213f
Darragh Redmond <darraghredmond7@gmail.com>
2024-02-25 17:04:37 +0000

    Added popup function + search bar and container.

--- DB copy.js
+++ DB copy.js
@@ -1,3 +1,4 @@
+// Function for adding markers to the map for each station. 
 function addBikeStationMarkers(map, stations) {
     
     const icon = {
@@ -5,6 +6,7 @@ function addBikeStationMarkers(map, stations) {
     scaledSize: new google.maps.Size(30, 30),
     };
     
+    // Loop through the stations array and add a marker for each station.
     stations.forEach(station => {
         const latLng = new google.maps.LatLng(station[4], station[5]);
         const marker = new google.maps.Marker({
@@ -13,6 +15,21 @@ function addBikeStationMarkers(map, stations) {
             title: station[2],
             icon: icon
         });
+   
+        // Event listiner to display a popup when marker is clicked. 
+        google.maps.event.addListener(marker, 'click', function() {
+            popup(marker, station, map);
+        });
     });
+}
+
+// Function to display a popup when a marker is clicked.
+// Can edit the contents of the popup later here.
+function popup(marker, station, map) {
+    const pupContent = `<div><h3>${station[2]}</h3><p>We can add real time data in here later</p></div>`;
+    const infoWindow = new google.maps.InfoWindow({
+        content: pupContent
+    });
+    infoWindow.open(map, marker);   
 }
 
