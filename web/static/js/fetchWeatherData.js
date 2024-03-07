741a0e63204900b5957d0a980aaade2ba9697383
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-03-07 12:31:08 +0000

    step1 to adding the weather - need to fix sizing and image set

--- web/static/js/fetchWeatherData.js
+++ web/static/js/fetchWeatherData.js
@@ -5,8 +5,32 @@ function fetchWeatherData() {
         .then(response => response.json())  // Parse the JSON response
         .then(data => {
             // Handle the data returned from the Flask API
-            console.log("weather")
-            document.getElementById('top-right').innerHTML = data.message;
+            console.log(data)
+            switch (data.Weather){
+                case "Clouds":
+                    weatherImage = "static/img/weatherImages/clouds.jpg"
+                    break;
+                case "Rain":
+                    weatherImage = "static/img/weatherImages/rainy.jpg"
+                    break;
+                case "Clear":
+                    weatherImage = "static/img/weatherImages/clear.jpg"
+                    break;
+                case "Mist":
+                    weatherImage = "static/img/weatherImages/mist.png"
+                    break;
+                case "Drizzle":
+                    weatherImage = "static/img/weatherImages/drizzle.png"
+                    break;
+                case "Snow":
+                    weatherImage = "static/img/weatherImages/snow.jpg"
+                    break;
+                default:
+                    weatherImage = "static/img/weatherImages/neatral.jpg"
+                    
+            }
+                
+            document.getElementById('weatherImage').src = weatherImage;
             
         })
         .catch(error => console.error('Error fetching data:', error));  // Handle any errors
