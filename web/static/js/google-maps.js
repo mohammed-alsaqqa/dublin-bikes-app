aa64051c2e313047e6ec9f81578ddd4b7e109b98
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-03-29 11:48:06 +0000

    Add closestThreeStations function to calculate and display the three closest bike stations to the search

--- web/static/js/google-maps.js
+++ web/static/js/google-maps.js
@@ -23,6 +23,15 @@ function initMap() {
   let constPlaceStart;
   let constPlaceFin;
 
+  // Fetching the static JSON with station data.
+  fetch("/stations_json_data")
+    .then((response) => response.json())
+    .then((data) => {
+      addBikeStationMarkers(map, data);
+      updateMarker(map, data);
+    })
+    .catch((error) => console.error("Error loading JSON data:", error));
+
   autocomplete.addListener("place_changed", function () {
     const place = autocomplete.getPlace();
     if (!place.geometry) {
@@ -37,6 +46,8 @@ function initMap() {
     }
 
     constPlaceStart = place.geometry.location;
+    closestThreeStations(constPlaceStart, "checkboxes-start");
+
   });
 
   const inputFin = document.getElementById("map-search-fin");
@@ -57,14 +68,45 @@ function initMap() {
     }
 
     constPlaceFin = placeFin.geometry.location;
+    closestThreeStations(constPlaceFin, "checkboxes-fin");
   });
+}
 
+function manhattanDistance(lat1, lon1, lat2, lon2) {
+  let a = Math.abs(lat1 - lat2);
+  let b = Math.abs(lon1 - lon2);
+  return a + b;
+}
+
+function closestThreeStations(location, htmlID) {
   // Fetching the static JSON with station data.
   fetch("/stations_json_data")
     .then((response) => response.json())
     .then((data) => {
-      addBikeStationMarkers(map, data);
-      updateMarker(map, data);
+      let distances = [];
+      data.forEach((station) => {
+        let stationLatLng = new google.maps.LatLng(
+          station.position_lat,
+          station.position_lng
+        );
+        let distance = google.maps.geometry.spherical.computeDistanceBetween(
+          location,
+          stationLatLng
+        );
+        distances.push({ distance: distance, station: station });
+      });
+      distances.sort((a, b) => a.distance - b.distance);
+      //   take three smallest distances
+      let closestStations = distances.slice(0, 3);
+
+      // add three distances as radar switches to the html in the div checkboxes start
+      let checkboxes = document.getElementById(htmlID);
+      checkboxes.innerHTML = ""; // Clear previous checkboxes
+      closestStations.forEach((item) => {
+        let div = document.createElement("div");
+        div.innerHTML = `<input type="radio" name="start" value="${item.station.station_name}">${item.station.station_name}`;
+        checkboxes.appendChild(div);
+      });
     })
     .catch((error) => console.error("Error loading JSON data:", error));
 }
