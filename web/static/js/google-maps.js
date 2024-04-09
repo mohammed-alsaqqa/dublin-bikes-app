9d87748a6b860507dfd76f34b606719bdea0adc7
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-04-09 10:14:10 +0100

    added the times in in a dropdown

--- web/static/js/google-maps.js
+++ web/static/js/google-maps.js
@@ -72,10 +72,8 @@ function initMap() {
   });
 }
 
-function manhattanDistance(lat1, lon1, lat2, lon2) {
-  let a = Math.abs(lat1 - lat2);
-  let b = Math.abs(lon1 - lon2);
-  return a + b;
+function linearDistance(lat1, lon1, lat2, lon2) {
+  return Math.sqrt(Math.pow(lat2 - lat1, 2) + Math.pow(lon2 - lon1, 2));
 }
 
 function closestThreeStations(location, htmlID) {
@@ -85,7 +83,7 @@ function closestThreeStations(location, htmlID) {
     .then((data) => {
       let distances = [];
       data.forEach((station) => {
-        let distance = manhattanDistance(station.position_lat, station.position_long, location.lat(), location.lng());
+        let distance = linearDistance(station.position_lat, station.position_long, location.lat(), location.lng());
 
         distances.push({ distance: distance, station: station });
       });
