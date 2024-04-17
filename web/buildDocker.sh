7ed7a219a2d9275bf033029f2da07c7ff3ce1344
mhmckeon <147508900+mhmckeon@users.noreply.github.com>
2024-04-17 14:40:44 +0100

    Delete web/buildDocker.sh

--- web/buildDocker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-podman build -t hello-flask:1.0.0 .
-if [ $? -ne 0 ]; then
-    echo "Build failed, trying with updated requirements..."
-    # Here you can add logic to modify requirements.txt, for example:
-    sed -i 's/==/>=/' requirements.txt
-    podman build -t hello-flask:1.0.0 .
-fi
